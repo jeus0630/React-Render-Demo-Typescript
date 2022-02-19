f6e00542f111ee269b238049783e92395a47df4f
Jewoo <jeus0630@gmail.com>
2022-02-19 12:56:35 +0900

    update: ParetFour.tsx
    useMemo, useCallback

--- src/components/Incorrect Optimizations/ParentFour.tsx
+++ src/components/Incorrect Optimizations/ParentFour.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, useMemo } from 'react';
 import ChildFive from './ChildFive';
 
 
@@ -18,12 +18,16 @@ const ParentFour: FunctionComponent<Props> = () => {
         lname: 'Wayne'
     }
 
+    const memoizedPerson = useMemo(()=>{
+        return person;
+    },[]);
+
     console.log('ParentFur Render');
     return (  
         <div>
             <button onClick={() => setCount(prev => prev + 1)}>Count - {count}</button>
             <button onClick={() => setName('codevolution')}>Change Name</button>
-            <ChildFive name={name} person={person}></ChildFive>
+            <ChildFive name={name} person={memoizedPerson}></ChildFive>
             {/* <ChildThree name={name}>
                 <strong>Hello</strong>
             </ChildThree> */}      
